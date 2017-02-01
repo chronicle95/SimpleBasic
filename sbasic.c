45e1fe089704534d6b8da50b2718635447b7545b
Artem Bondarenko <artembondarenko@i.ua>
2017-02-01 03:24:42 +0200

    Separated configuration defines

--- sbasic.c
+++ sbasic.c
@@ -1,19 +1,7 @@
 #include <stdio.h>
 
-#define MAX_LINE 10000
-#define NO_LINE -1
-
-#define VAR_NAMESZ 10
-#define VAR_COUNT 64
-#define CMD_NAMESZ 10
-#define CMD_COUNT 10
-#define DATA_STSZ 16
-#define CALL_STSZ 16
-#define MEMORY_SZ 100
-#define LINE_SZ 80
-#define CODE_SZ 4096
-
 /* Do not change the order of inclusion */
+#include "sbconf.h"
 #include "sbmisc.c"
 #include "sbstruct.c"
 #include "sbparse.c"
---
+++ sbconf.h
@@ -0,0 +1,13 @@
+
+#define MAX_LINE 10000
+#define NO_LINE -1
+
+#define VAR_NAMESZ 10
+#define VAR_COUNT 64
+#define CMD_NAMESZ 10
+#define CMD_COUNT 10
+#define DATA_STSZ 16
+#define CALL_STSZ 16
+#define MEMORY_SZ 100
+#define LINE_SZ 80
+#define CODE_SZ 4096
